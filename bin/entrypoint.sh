9b93cbaf6a0a257c8db326547b99a22da79b91c4
Lee Myring <3706672+deploymentking@users.noreply.github.com>
2026-08-07 15:12:47 +0100

    TEL-5695 migrate to mise and uv (#38)

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,46 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-### WARNING! This is a generated file and should ONLY be edited in https://github.com/hmrc/telemetry-lambda-resources
-
-set -xeu
-
-# Debug Python environment
-python --version
-pip --version
+set -eu
 
 # Initialise directories
 BASEDIR=/data
 cd ${BASEDIR}
 
 # Force Debian to use HTTPS
+cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.bak
 sed --in-place 's|http://|https://|g' /etc/apt/sources.list.d/debian.sources
 
 # Update the package listing, so we know what package exist:
-apt-get update
+apt-get update && apt-get -y upgrade && apt-get install -y libssl-dev zip
 
-# Install security updates:
-apt-get -y upgrade
-
-# Install test requirements
+# Install requirements
 python -m venv "${VENV_NAME}"
-source ./"${VENV_NAME}"/bin/activate
+source "${VENV_NAME}/bin/activate"
 
 # The platform argument ensures greater compatibility with AWS Lambda Runtimes
 # https://repost.aws/knowledge-center/lambda-python-package-compatible
 # https://github.com/pypa/manylinux - The chosen platform will be EOL June 2024
-pip install --index-url https://artefacts.tax.service.gov.uk/artifactory/api/pypi/pips/simple --upgrade pip
 
 # force reinstall cryptography to ensure it uses rust utils
-pip install --index-url https://artefacts.tax.service.gov.uk/artifactory/api/pypi/pips/simple --force-reinstall cryptography
+pip install --index-url "${PIP_INDEX_URL}" --force-reinstall cryptography
 
-pip install --index-url https://artefacts.tax.service.gov.uk/artifactory/api/pypi/pips/simple \
-            --requirement "${REQUIREMENTS_FILE}" \
-            --platform manylinux2014_x86_64 \
-            --target=./${VENV_NAME}/lib/python3.14/site-packages \
+pip install --requirement "${REQUIREMENTS_FILE}" \
             --implementation cp \
-            --only-binary=:all:
+            --index-url "${PIP_INDEX_URL}" \
+            --only-binary=:all: \
+            --platform "manylinux_2_28_x86_64" \
+            --platform "manylinux2014_x86_64" \
+            --target="./${VENV_NAME}/lib/python${PYTHON_VERSION_LIB}/site-packages"
 
 # Make the binary location specified in --target above, available to PATH
-export PATH="$PATH:./${VENV_NAME}/lib/python3.14/site-packages/bin"
+export PATH="$PATH:./${VENV_NAME}/lib/python${PYTHON_VERSION_LIB}/site-packages/bin"
 
 exec "$@"
